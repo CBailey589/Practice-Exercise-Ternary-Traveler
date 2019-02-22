f9e82972ec608e2eec3dfeb05d94be66d010a0b1
Connor Bailey <bailey.connor.p@gmail.com>
2019-02-22 16:33:50 -0600

    fixed opacity issue for alert boxes?

--- src/scripts/createHeader.js
+++ src/scripts/createHeader.js
@@ -8,40 +8,46 @@ function createHeader() {
             <div id="newInterestHeader">Want to add a new point of interest?</div>
             <button id="startNewInterest">Click Here!</button>
         </div>
-        <div id="newInterestForm" class="hidden">
-            <fieldset>
-                <label for="interestPlace"></label>
-                <select name="interestPlace" id="interestPlace"></select>
-            </fieldset>
-            <fieldset>
-                <label for="interestName">Name of Place of Interest:</label>
-                <input type="text" name="interestName" id="interestName">
-            </fieldset>
-            <fieldset>
-                <label for="interestLocation">Location of Place of Interest:</label>
-                <input type="text" name="interestLocation" id="interestLocation">
-            </fieldset>
-            <fieldset>
-                <label for="interestDescription"></label>
-                <textarea name="interestDescription" id="interestDescription" cols="30" rows="10" placeHolder="Enter a description of the place of interest here..."></textarea>
-            </fieldset>
-            <button id="cancelNewInterest">Cancel</button>
-            <button id="submitNewInterest">Submit</button>
-        </div>
-        <div id="doubleCheckDeleteSection" class="hidden">
-            <input type="hidden" value="" id="idToDelete"
-            <h1 id="deleteHeader">Are you sure you want to REMOVE this place of interest?</h1>
-            <div id="deleteInterestName"></div>
-            <button id="cancelDelete">Cancel</button>
-            <button id="delete">BE GONE!</button>
-        </div>
     </section>
     `
     printToDOM(string, "#output")
+
+    string = `
+        <section id="alertStuff"
+            < div id = "newInterestForm" class="hidden" >
+                <fieldset>
+                    <label for="interestPlace"></label>
+                    <select name="interestPlace" id="interestPlace"></select>
+                </fieldset>
+                <fieldset>
+                    <label for="interestName">Name of Place of Interest:</label>
+                    <input type="text" name="interestName" id="interestName">
+                </fieldset>
+                    <fieldset>
+                        <label for="interestLocation">Location of Place of Interest:</label>
+                        <input type="text" name="interestLocation" id="interestLocation">
+                </fieldset>
+                <fieldset>
+                    <label for="interestDescription"></label>
+                    <textarea name="interestDescription" id="interestDescription" cols="30" rows="10" placeHolder="Enter a description of the place of interest here..."></textarea>
+                </fieldset>
+                <button id="cancelNewInterest">Cancel</button>
+                <button id="submitNewInterest">Submit</button>
+            </div>
+            <div id="doubleCheckDeleteSection" class="hidden">
+                        <input type="hidden" value="" id="idToDelete"
+                <h1 id="deleteHeader">Are you sure you want to REMOVE this place of interest?</h1>
+                    <div id="deleteInterestName"></div>
+                    <button id="cancelDelete">Cancel</button>
+                    <button id="delete">BE GONE!</button>
+            </div>
+        </section>
+        `
+    printToDOM(string, "#alert")
     return API.GET("places")
         .then((res) => res.map(place => {
             let string = `
-            <option value="${place.id}">${place.name}</option>
+        < option value = "${place.id}" > ${place.name}</option >
             `
             printToDOM(string, "#interestPlace")
         }))
